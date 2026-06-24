dfb961b28e4186e4c45fe52c839a6ae584ce8609
abdulrahman417 <abdulrahmandawshi2@gmail.com>
2026-06-24 22:14:25 +0300

    Delete futuristic-portfolio-v2/dist directory

--- futuristic-portfolio-v2/dist/server/assets/_tanstack-start-manifest_v-u2nSQVfw.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/User/Desktop/Portfolio/Lovable/Portfolio/futuristic-portfolio-v2/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CQUJDvPZ.js"] }, "/": { filePath: "C:/Users/User/Desktop/Portfolio/Lovable/Portfolio/futuristic-portfolio-v2/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BBrxFwBd.js"] } }, clientEntry: "/assets/index-CQUJDvPZ.js" });
-export {
-  tsrStartManifest
-};
